569636f8bcd3cd809a81cf1ae18a1bc5288fe0c4
Keith Jackson <kj5thguitar@gmail.com>
2017-02-10 15:12:57 -0500

    Added 2nd class

---
+++ MVC Review Site Project/Models/GenreType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site_Project.Models
+{
+    public class GenreType
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Genre { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
--- MVC Review Site Project/Models/Review.cs
+++ MVC Review Site Project/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace MVC_Review_Site_Project.Models
 {
     public class Review
     {
+        [Key]
+        public int ID { get; set; }
+        public string AlbumName {get; set;}
+        public string AlbumReview { get; set; }
+        public double Rating { get; set; }
+
+        [ForeignKey("Genre")]
+        public int GenreID { get; set; }
+        public virtual GenreType Genre { get; set; }
+
     }
 }
